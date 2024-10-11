0b5f6befc6cbe6ac70b5f46fb73ba1f1cf397276
Coding Ninjas 10x Dev Club <cndc@codingninjas.com>
2024-10-11 09:57:07 +0530

    customers.js

--- client/src/components/customers/customers.js
+++ client/src/components/customers/customers.js
@@ -5,25 +5,33 @@ class Customers extends React.Component {
   constructor() {
     super();
     this.state = {
-      customers: [],
+      customers: [], 
+      loading: true,
     };
   }
+
   componentDidMount() {
     fetch("/api/customers")
       .then((res) => res.json())
       .then((customers) =>
-        this.setState({ customers }, () =>
-          console.log("Customers fetched!", customers)
-        )
-      );
+        this.setState({ customers })
+      )
+      .catch((error) => {
+        console.error("Error fetching customers", error);
+        this.setState({ customers: [] });
+      });
   }
+
   render() {
     return (
       <div>
         <h1>F.R.I.E.N.D.S</h1>
         <ul>
-          {this.state.customers.map((customer) => (
-            <li key={customer.id}>{customer.name}</li>
+          {this.state.customers.length === 0 && (
+            <p>No customers available</p>
+          )}
+          {this.state.customers.map((customer, index) => (
+            <li key={index}>{customer.name}</li>
           ))}
         </ul>
       </div>
