bf8c46ce87b97930fcf1fd1f88b083acf307cdfb
syamjir <rsyamjir@gmail.com>
2024-10-12 13:07:13 +0530

    Add loading state to Customers component

--- client/src/components/customers/customers.js
+++ client/src/components/customers/customers.js
@@ -5,20 +5,20 @@ class Customers extends React.Component {
   constructor() {
     super();
     this.state = {
-      customers: [], 
-      loading: true,
+      customers: [],
+      loading: true, // Initially set loading to true
     };
   }
 
   componentDidMount() {
     fetch("/api/customers")
       .then((res) => res.json())
-      .then((customers) =>
-        this.setState({ customers })
+      .then(
+        (customers) => this.setState({ customers, loading: false }) // Set loading to false once data is fetched
       )
       .catch((error) => {
         console.error("Error fetching customers", error);
-        this.setState({ customers: [] });
+        this.setState({ customers: [], loading: false }); // Set loading to false even on error
       });
   }
 
@@ -26,14 +26,19 @@ class Customers extends React.Component {
     return (
       <div>
         <h1>F.R.I.E.N.D.S</h1>
-        <ul>
-          {this.state.customers.length === 0 && (
-            <p>No customers available</p>
-          )}
-          {this.state.customers.map((customer, index) => (
-            <li key={index}>{customer.name}</li>
-          ))}
-        </ul>
+        {this.state.loading ? (
+          <p>Loading customers...</p> // Show loading message while fetching
+        ) : (
+          <ul>
+            {this.state.customers.length === 0 ? (
+              <p>No customers available</p> // Show this message if no customers are returned
+            ) : (
+              this.state.customers.map((customer, index) => (
+                <li key={index}>{customer.name}</li>
+              ))
+            )}
+          </ul>
+        )}
       </div>
     );
   }
